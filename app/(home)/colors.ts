125d1d06162b88aa438cce362a044dc6ef145ad8
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2025-04-04 06:11:52 -0400

    feat: added giscus support for comments, more color options for gradient animation

--- app/(home)/colors.ts
+++ app/(home)/colors.ts
@@ -21,6 +21,25 @@ const TUNDORA = [75, 75, 75] // Graphite, Hex = #4B4B4B
 const PINE_GREEN2 = [0, 110, 120] // Hex = #006E78
 const PERSIAN_GREEN = [0, 179, 152] // Hex = #00B398
 
+// Night sky - Blue satin
+const INDIGO = [72, 124, 198] // Hex = #487CC6
+const CELLO = [36, 62, 99] // Hex = #243e63
+const FIREFLY = [10, 20, 35] // Hex = #0a1423
+const ASTRONAUT = [0, 179, 152] // Hex = #27507a #204369
+
+// Purple - Reds
+const MARTINIQUE = [48, 44, 80] // Hex = #302c50
+const DARK_MONZA_RED = [179, 0, 30] // Hex = #B3001E
+
+// Great combinations:
+// A: Malachite, B: Paradiso
+// A: Firefly, B: Cello or Indigo or Astronaut.
+// A: Martinique, B: Cello for a blue gradient, Dark Monza Red for a blue-red mix.
+
+// Tips:
+// 1. Color A is dominant, B is accent.
+// 2. It's generally better to use a darker color for Color A.
+
 // Selection
 export const COLOR_A = MALACHITE
 export const COLOR_B = PARADISO
